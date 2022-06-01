8dba46e0e9c4b7037b2ad55267f757e71eba68ff
Matoskipo <98059518+Matoskipo@users.noreply.github.com>
2022-06-01 21:45:24 +0100

    "my first commit"

--- practice-name.js
+++ practice-name.js
@@ -1,6 +1,9 @@
- module.exports.items = ['item1', 'item2']
-
-const singlePerson ={
-    name: 'Matthew',
-}
-module.exports.person = singlePerson
+var shuffle = function(nums, n) {
+   let newVals = []
+   for(let i = 0; i < n; i ++)
+   {
+       newVals.push(nums[i], nums[i+n])
+   }
+   return newVals
+};
+console.log(shuffle([2,5,1,3,4,7],3))
